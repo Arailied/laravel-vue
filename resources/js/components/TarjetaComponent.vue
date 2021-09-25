513cfa7909c421c634f6f6db4ddc2f88709e3294
Arailied <avfcorm@gmail.com>
2021-09-25 10:56:03 -0400

    primer cambio

---
+++ resources/js/components/TarjetaComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <div class="fondo">
+        <h1>hola</h1>
+
+        <p>parrafo de ejemplo</p>
+    </div> -->
+
+  <div class="card" style="width: 18rem">
+    <img src="https://fundacioncompartir.org/sites/default/files/la-nueva-tecnologia-que-esta-moviendo-al-mundo.jpg" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* h1{
+        color: #f00;
+    }
+
+    .fondo{
+        background-color: blueviolet;
+    }
+    p{
+        color:#fff;
+    } */
+
+    .card{
+        float: left;
+    }
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="/css/app.css">
+
+
+<h1>Renderizando componentes</h1>
+
+<div id ="app">
+
+<hola></hola>
+<hola></hola>
+
+<hola></hola>
+<hola></hola>
+<hola></hola>
+<hola></hola>
+
+</div>
+
+<script src="/js/app.js"></script>
